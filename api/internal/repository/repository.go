91bc149a56ee39da0ef52b2893347d780a8a44a4
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-06-23 20:30:07 +0800

    feat: add user reporitory

--- api/internal/repository/repository.go
+++ api/internal/repository/repository.go
@@ -1,3 +1,21 @@
 package repository
 
+import (
+	"context"
 
+	"github.com/ivanpaghubasan/hoa-hub/internal/model"
+	"github.com/jmoiron/sqlx"
+)
+
+type Repository struct {
+	User interface {
+		CreateUser(ctx context.Context, user *model.User) (*model.User, error)
+		GetUserByEmail(ctx context.Context, email string) (*model.User, error)
+	}
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		User: NewUserRepository(db),
+	}
+}
