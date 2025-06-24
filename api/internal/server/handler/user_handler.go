3d0faa79d145645bfba399ec174b048cceee8c8d
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-06-24 12:50:18 +0800

    feat: add request and response struct for creating a user

--- api/internal/server/handler/user_handler.go
+++ api/internal/server/handler/user_handler.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/ivanpaghubasan/hoa-hub/internal/service"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/ivanpaghubasan/hoa-hub/internal/service"
+)
 
 type UserHandler struct {
 	userService service.UserService
@@ -10,4 +15,18 @@ func NewUserHandler(service service.UserService) *UserHandler {
 	return &UserHandler{userService: service}
 }
 
+func (handler *UserHandler) CreateUser(c *gin.Context) {
+	var request service.CreateUserRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
 
+	response, err := handler.userService.CreateUser(c.Request.Context(), &request)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"data": response})
+}
--- api/internal/service/service.go
+++ api/internal/service/service.go
@@ -15,9 +15,20 @@ type Service struct {
 }
 
 type CreateUserRequest struct {
+	FirstName  string  `json:"first_name" binding:"reqiured"`
+	LastName   string  `json:"last_name" binding:"required"`
+	MiddleName *string `json:"middle_name"`
+	Email      string  `json:"email" binding:"required,email"`
+	Password   string  `json:"password" binding:"required,min=8"`
+	UserType   string  `json:"user_type" binding:"required"`
 }
 
 type CreatUserResponse struct {
+	FirstName  string  `json:"first_name"`
+	LastName   string  `json:"last_name"`
+	MiddleName *string `json:"middle_name"`
+	Email      string  `json:"email"`
+	UserType   string  `json:"user_type"`
 }
 
 func NewService(repos *repository.Repository) *Service {
