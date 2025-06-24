b74b3ce6ff452fa058674ec9cf0dc4033b0f7635
Ivan Paghubasan <ivan.paghubasan@hoolisoftware.com>
2025-06-24 14:08:14 +0800

    refactor: update user model

--- api/internal/model/user.go
+++ api/internal/model/user.go
@@ -7,12 +7,24 @@ import (
 )
 
 type User struct {
+	ID           uuid.UUID  `db:"id"`
+	FirstName    string     `db:"first_name"`
+	LastName     string     `db:"last_name"`
+	MiddleName   *string    `db:"middle_name"`
+	DateOfBirth  *time.Time `db:"date_of_birth"`
+	Gender       string     `db:"gender"`
+	MobileNumber string     `db:"mobile_number"`
+	Status       string     `db:"status"`
+	CreatedAt    time.Time  `db:"created_at"`
+	UpdatedAt    time.Time  `db:"updated_at"`
+}
+
+type Account struct {
 	ID           uuid.UUID `db:"id"`
-	FirstName    string    `db:"first_name"`
-	LastName     string    `db:"last_name"`
-	MiddleName   *string   `db:"middle_name"`
+	UserID       uuid.UUID `db"user_id"`
 	Email        string    `db:"email"`
 	PasswordHash string    `db:"password_hash"`
+	Status       string    `db:"status"`
 	CreatedAt    time.Time `db:"created_at"`
 	UpdatedAt    time.Time `db:"updated_at"`
 }
